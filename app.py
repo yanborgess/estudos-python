2e50904cb74fded7aceb5c4aefd046b327dfca0d
yanborgess <yan.borges.areda@gmail.com>
2025-08-26 20:05:47 -0300

    estudos

---
+++ app.py
@@ -0,0 +1,30 @@
+# VARIÁVEIS
+total = []
+totalfem = 0
+totalmasc = 0
+mediamasc = []
+
+for i in range(4):
+    alt = float(input("Digite sua altura: "))
+    total.append(alt)
+    sexo = input("Qual seu gênero: MASCULINO (M) FEMININO (F): ").upper()
+
+    if sexo == "F":
+        totalfem += 1
+    elif sexo == "M":
+        mediamasc.append(alt)
+        totalmasc += 1
+
+# Maior e menor altura do grupo
+print(f"A maior altura do grupo é de: {max(total)} m")
+print(f"A menor altura do grupo é de: {min(total)} m")
+
+# Média de altura dos homens
+if totalmasc > 0:
+    media = sum(mediamasc) / totalmasc
+    print(f"A média de altura dos homens do grupo é igual a: {media:.2f} m")
+else:
+    print("Não tem homens no grupo.")
+
+# Quantidade de mulheres
+print(f"A quantidade de mulheres no grupo é igual a: {totalfem}")
